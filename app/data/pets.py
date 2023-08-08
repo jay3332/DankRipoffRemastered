31cb3d7ed23b31f7c470f32bb01871fa93226f09
jay3332 <40323796+jay3332@users.noreply.github.com>
2023-08-08 14:30:41 -0400

    make pet energy drain slower

--- app/data/pets.py
+++ app/data/pets.py
@@ -20,7 +20,7 @@ class Pet(NamedTuple):
     emoji: str
     rarity: PetRarity
     description: str
-    energy_per_minute: int
+    energy_per_minute: float
     max_energy: int
     benefit: Callable[[int], str]  # Passive
     abilities: Callable[[int], str] | None = None  # Active
@@ -53,7 +53,7 @@ class Pets:
         emoji='<:dog:1134641292245205123>',
         rarity=PetRarity.common,
         description="A descendant of the wolf and a man's best friend.",
-        energy_per_minute=1,
+        energy_per_minute=0.5,
         max_energy=100,
         benefit=lambda level: (
             f'- +{1 + level * 0.3}% coins from begging\n'
@@ -67,7 +67,7 @@ class Pets:
         emoji='<:cat:1134641341092089948>',
         rarity=PetRarity.common,
         description='A small, domesticated, carnivorous mammal.',
-        energy_per_minute=1,
+        energy_per_minute=0.5,
         max_energy=100,
         benefit=lambda level: (
             f'- +{1 + level * 0.2}% weight on finding rarer items when fishing\n'
@@ -81,7 +81,7 @@ class Pets:
         emoji='\U0001f426',
         rarity=PetRarity.common,
         description='Birb. These can fly, in case you were clueless.',
-        energy_per_minute=1,
+        energy_per_minute=0.5,
         max_energy=100,
         benefit=lambda level: (
             f'- +{1 + level * 0.4}% global coin multiplier'
@@ -94,7 +94,7 @@ class Pets:
         emoji='\U0001f430',
         rarity=PetRarity.common,
         description='A mammal with long ears that hops around.',
-        energy_per_minute=1,
+        energy_per_minute=0.5,
         max_energy=100,
         benefit=lambda level: (
             f'- +{1 + level * 0.5}% global XP multiplier'
@@ -107,7 +107,7 @@ class Pets:
         emoji='\U0001f439',
         rarity=PetRarity.common,
         description='A small rodent that is often kept as a pet.',
-        energy_per_minute=1,
+        energy_per_minute=0.5,
         max_energy=100,
         benefit=lambda level: (
             f'- +{1 + level * 0.4}% weight on finding rarer items when digging\n'
@@ -135,7 +135,7 @@ class Pets:
         emoji='\U0001f986',
         rarity=PetRarity.uncommon,
         description='Waddle waddle and then they go quack',
-        energy_per_minute=2,
+        energy_per_minute=0.75,
         max_energy=200,
         benefit=lambda level: (
             f'- +{2 + level * 0.5}% profit from working\n'  # TODO
@@ -150,7 +150,7 @@ class Pets:
         emoji='\U0001f41d',
         rarity=PetRarity.uncommon,
         description='A flying insect that pollinates flowers and makes honey.',
-        energy_per_minute=2,
+        energy_per_minute=0.75,
         max_energy=300,
         benefit=lambda level: (
             f'- +{1 + level * 0.4}% faster harvesting crops\n'
@@ -167,7 +167,7 @@ class Pets:
         emoji='\U0001f42e',
         rarity=PetRarity.rare,
         description='A large mammal used for producing milk (and steak of course).',
-        energy_per_minute=3,
+        energy_per_minute=1.25,
         max_energy=500,
         benefit=lambda level: (
             f'- +{2 + level * 0.5}% more coins from beg, search, and crime\n'
