f7c507a41e923fd092fed74ab88408a1b4b176d0
jay3332 <40323796+jay3332@users.noreply.github.com>
2023-08-23 14:27:15 -0400

    fix help command

--- app/extensions/combat.py
+++ app/extensions/combat.py
@@ -4,5 +4,8 @@ from app.core import Cog
 class Combat(Cog):
     """Commands related to training and participating in combat and battling."""
 
+    emoji = '\u2694\ufe0f'
+    __hidden__ = True  # TODO Remove
+
 
 setup = Combat.simple_setup
